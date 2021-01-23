2948b372cafc896c790dc1f564581391561c441b
goksalsarmusak <77418553+goksalsarmusak@users.noreply.github.com>
2021-01-23 23:52:27 +0300

    23 ocak

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager//iş sınıflarıdır.
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //İhtiyacKrediManager ihtiyacKrediManager = new İhtiyacKrediManager();
+            //ihtiyacKrediManager.Hesapla();
+
+            IKrediManager ihtiyacKrediManager = new İhtiyacKrediManager(); //bu şekilde de yazılabilir
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager,fileLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
